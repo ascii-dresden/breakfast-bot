9c91a3464087ec2c172d49ba15dc5ffc5fb4a893
Johannes Kliemann <jk@jkliemann.de>
2023-08-05 12:40:44 +0200

    Add configuration, separate Telegram

--- breakfastbot.py
+++ breakfastbot.py
@@ -7,6 +7,7 @@ import time
 import logging
 import shelve
 import schedule
+import configparser
 from telegram import Update, ChatMember
 from telegram.ext import Updater, CallbackContext, ChatMemberHandler, PollAnswerHandler
 
@@ -16,13 +17,13 @@ updater: Updater = None
 state: shelve.Shelf = None
 
 def chat_member_callback(update: Update, context: CallbackContext):
-    chats = state["chats"]
+    chats = state["telegram"]["chats"]
     member = update.my_chat_member.new_chat_member
     if member["status"] in (ChatMember.MEMBER, ChatMember.CREATOR, ChatMember.ADMINISTRATOR):
         chats.add(update.effective_chat.id)
     elif update.effective_chat.id in chats:
         chats.remove(update.effective_chat.id)
-    state["chats"] = chats
+    state["telegram"]["chats"] = chats
 
 def sighandler(signum, frame):
     state.close()
@@ -36,46 +37,58 @@ def run_scheduler():
         time.sleep(60)
 
 def start_poll():
-    polls = state["polls"]
+    polls = state["telegram"]["polls"]
     logging.info("Notifying")
-    for chat in state["chats"]:
-        options = ["Ja", "Ja, keine Brötchen", "Nein :("]
-        poll = updater.bot.send_poll(chat_id=chat,
-                                     question="Bist du morgen beim Frühstück dabei?",
-                                     options=options,
-                                     is_anonymous=False,
-                                     allows_multiple_answers=False)
-        polls[poll["poll"]["id"]] = [poll["chat"]["id"], poll["message_id"], {}]
-    state["polls"] = polls
+    if updater:
+        for chat in state["telegram"]["chats"]:
+            options = ["Ja", "Ja, keine Brötchen", "Nein :("]
+            poll = updater.bot.send_poll(chat_id=chat,
+                                         question="Bist du morgen beim Frühstück dabei?",
+                                         options=options,
+                                         is_anonymous=False,
+                                         allows_multiple_answers=False)
+            polls["telegram"][poll["poll"]["id"]] = [poll["chat"]["id"], poll["message_id"], {}]
+    for room in state["matrix"]["rooms"]:
+        pass
+    state["telegram"]["polls"] = polls
 
 def finish_poll():
-    polls = state["polls"]
-    for poll in polls.values():
-        try:
-            updater.bot.stop_poll(chat_id=poll[0], message_id=poll[1])
+    polls = state["telegram"]["polls"]
+    if updater:
+        for poll in polls.values():
+            try:
+                updater.bot.stop_poll(chat_id=poll[0], message_id=poll[1])
 
-            # count the number of users who want bread
-            participant_count = len([option_ids for option_ids in poll[2].values() if 0 in option_ids])
+                # count the number of users who want bread
+                participant_count = len([option_ids for option_ids in poll[2].values() if 0 in option_ids])
 
-            bread_count = int(participant_count * 2 - participant_count / 4)
-            updater.bot.send_message(chat_id=poll[0], text=f"Brötchen: {bread_count}")
-        except:
-            pass
-    state["polls"] = {}
+                bread_count = int(participant_count * 2 - participant_count / 4)
+                updater.bot.send_message(chat_id=poll[0], text=f"Brötchen: {bread_count}")
+            except:
+                pass
+    state["telegram"]["polls"] = {}
 
 def poll_answer_callback(update: Update, context: CallbackContext):
-    polls = state["polls"]
+    polls = state["telegram"]["polls"]
     answer=update.poll_answer
 
     # store the last answer for each user_id
     polls[answer["poll_id"]][2].update({answer["user"]["id"]: answer["option_ids"]})
-    state["polls"] = polls
+    state["telegram"]["polls"] = polls
 
 def initialize_state():
-    if "chats" not in state:
-        state["chats"] = set()
-    if "polls" not in state:
-        state["polls"] = {}
+    if "telegram" not in state:
+        state["telegram"] = {}
+        if "chats" in state:
+            state["telegram"]["chats"] = state["chats"]
+        else:
+            state["telegram"]["chats"] = set()
+        if "polls" in state:
+            state["telegram"]["polls"] = state["polls"]
+        else:
+            state["telegram"]["polls"] = {}
+    if "matrix" not in state:
+        state["matrix"] = {"rooms": set(), "polls" : {}}
 
 def main(args):
     global state
@@ -83,16 +96,19 @@ def main(args):
     if len(args) < 2:
         sys.exit(1)
     logging.info("Starting breakfast bot")
+    config = configparser.ConfigParser()
+    config.read(args[1])
     state = shelve.open("breakfastbot")
     initialize_state()
     signal.signal(signal.SIGINT, sighandler)
     signal.signal(signal.SIGTERM, sighandler)
     schedule.every().thursday.at("10:30").do(start_poll)
     schedule.every().friday.at("07:00").do(finish_poll)
-    updater = Updater(args[1])
-    updater.dispatcher.add_handler(ChatMemberHandler(chat_member_callback, ChatMemberHandler.MY_CHAT_MEMBER))
-    updater.dispatcher.add_handler(PollAnswerHandler(poll_answer_callback))
-    updater.start_polling()
+    if 'Telegram' in config:
+        updater = Updater(config['Telegram']['api_key'])
+        updater.dispatcher.add_handler(ChatMemberHandler(chat_member_callback, ChatMemberHandler.MY_CHAT_MEMBER))
+        updater.dispatcher.add_handler(PollAnswerHandler(poll_answer_callback))
+        updater.start_polling()
     logging.info("Bot started...")
     run_scheduler()
 
