9799089fb9ce54cdc0ec022a5c31c10dd07a05ae
Johannes Kliemann <jk@jkliemann.de>
2025-01-12 12:41:52 +0100

    Add missing optional job-queue dependency

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
         (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [
             schedule
             python-telegram-bot
-        ]))
+        ] ++ python-telegram-bot.optional-dependencies.job-queue))
     ];
     dontUnpack = true;
     installPhase = "install -Dm755 ${./breakfastbot.py} $out/bin/breakfastbot";
